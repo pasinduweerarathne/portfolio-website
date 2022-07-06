bb3c6757a7815763ec7c7155efc4bcb609a2c125
H.K.D.Pasindu Pramod Weerarathne <pasiweerarathne98@gmail.com>
2022-07-06 12:19:04 +0530

    update srollbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,20 @@ import About from "./pages/About";
 import Skills from "./pages/Skills";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
+import "./App.css";
 
 const App = () => {
+  let progress = document.getElementById("progressbar");
+  let totalHeight = document.body.scrollHeight - window.innerHeight;
+  window.onscroll = function () {
+    let progressHeight = (window.pageYOffset / totalHeight) * 100;
+    progress.style.height = progressHeight + "%";
+  };
+
   return (
     <div>
+      <div id="progressbar"></div>
+      <div id="scrollPath"></div>
       <NavBar />
       <Home />
       <About />
